ed07391bb93f622f7727158a10424e1a89538c6f
dio <dio@DESKTOP-99NH0K5>
2025-12-16 22:28:19 +0500

    Добавьте файлы проекта.

---
+++ AmericanBo/AmericanBo.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+bool Isnum(char sym)
+{
+	if (!std::isdigit(sym))
+	{
+		return false;
+	}
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	if (argc == 4 && Isnum(*argv[1]) && Isnum(*argv[3]) && *argv[2] == '+')
+	{
+		std::cout << "\n===============================================================\n";
+		std::cout << argv[1] << " " << argv[2] << " " << argv[3] << " = " << atoi(argv[1]) + atoi(argv[3]) << std::endl;
+	}
+	else if (argc == 4 && Isnum(*argv[1]) && Isnum(*argv[3]) && *argv[2] == '-')
+	{
+		std::cout << "\n===============================================================\n";
+		std::cout << argv[1] << " " << argv[2] << " " << argv[3] << " = " << atoi(argv[1]) - atoi(argv[3]) << std::endl;
+	}
+	else
+	{
+		std::cout << "\n===============================================================\n";
+		std::cout << "\tНеправильный ввод! Нужно Число \"Знак\" и Число \n";
+	}
+
+	system("pause"); 
+}
+
+
